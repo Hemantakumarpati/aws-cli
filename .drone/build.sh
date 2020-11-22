2be46f0575cb2a0776dedfb8f538ace86dbcec89
Hemantakumarpati <34638115+Hemantakumarpati@users.noreply.github.com>
2020-11-22 20:21:03 +0530

    Update build.sh

--- .drone/build.sh
+++ .drone/build.sh
@@ -7,7 +7,7 @@ wrapdocker &
 sleep 5
 
 docker build -t hemantakumarpati/awscli:latest . 
-docker login -u "hemantakumarpati" -p "Master@1927" docker.io
+docker login -u "" -p "" docker.io
 #docker tag tomcatonline hemantakumarpati/tomcatonetime:latest
 docker push hemantakumarpati/awscli:latest .
 docker container run -it -d --name awscli -p 5000:5000 hemantakumarpati/awscli
